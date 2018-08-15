115dd8a9a7ec1d2483d389f58fc952c833200f8c
Ankit Bhagat <acb693@nyu.edu>
2018-08-14 22:26:41 -0400

    Adding 4. Binomial Coeff

---
+++ BinomialC.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+class BinomialC{
+	private static int findBinomial(int n, int k){
+		int[][] dp = new int[n+1][k+1];
+
+		for(int i=0;i<=n;i++){
+			for(int j=0;j<=k;j++){
+				if(j==0 || j==i || i==0){
+					dp[i][j] = 1;
+				}
+				else{
+					dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+				}
+			}
+		}
+		return dp[n][k];
+	}
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		System.out.println(findBinomial(n,k));
+	}
+}
